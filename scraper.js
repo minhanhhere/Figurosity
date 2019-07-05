7d97c946e7b16523a707a8835fbd6405a6f4cb69
minhanhhere <minhanhhere@gmail.com>
2019-07-05 21:57:33 +0700

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -3,12 +3,14 @@
 var cheerio = require("cheerio");
 var request = require("request");
 var sqlite3 = require("sqlite3").verbose();
+var axios = require('axios');
 
 function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
 		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
+		db.run("CREATE TABLE IF NOT EXISTS pose (id NUMBER, uuid TEXT, render_count NUMBER)");
 		callback(db);
 	});
 }
@@ -56,4 +58,35 @@ function run(db) {
 	});
 }
 
-initDatabase(run);
+function fetchPose(db) {
+    for (let i = 1; i <= 2; i++) {
+        axios.post('https://api.figurosity.com/public/v1/poses', {"page": i,"models":[],"cameras":[],"gender":[],"style":[],"action":[],"props":[]})
+            .then(response => {
+                return fetchFromApi(db, response.data.poses.data);
+            })
+            .then(() => console.log('DONE Page ' + i));
+    }
+}
+
+function fetchFromApi(db, data) {
+    return Promise.all(data.map(p => convertAndSavePose(db, p)));
+}
+
+function convertAndSavePose(db, data) {
+    
+    var pose = {
+        id: data.id,
+        uuid: data.uuid,
+        render_count: data.render_count
+    };
+
+    console.log(`Save poses id=${data.id}`);
+
+    var statement = db.prepare("INSERT INTO data VALUES (?,?,?)");
+	statement.run(pose.id, pose.uuid, pose.render_count);
+	statement.finalize();
+
+    return pose;
+}
+
+initDatabase(fetchPose);
